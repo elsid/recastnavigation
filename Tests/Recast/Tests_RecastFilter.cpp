34e585947ae2713df3faca61cb0701800f63d60a
elsid <elsid.mail@gmail.com>
2025-04-13 14:22:59 +0200

    Add test for rcFilterLedgeSpans

--- Tests/Recast/Tests_RecastFilter.cpp
+++ Tests/Recast/Tests_RecastFilter.cpp
@@ -7,6 +7,17 @@
 #include "Recast.h"
 #include "RecastAlloc.h"
 
+namespace
+{
+	std::vector<unsigned> getAreas(const rcSpan* span)
+	{
+		std::vector<unsigned> result;
+		for (const rcSpan* v = span; v != NULL; v = v->next)
+			result.push_back(v->area);
+		return result;
+	}
+}
+
 TEST_CASE("rcFilterLowHangingWalkableObstacles", "[recast, filtering]")
 {
 	rcContext context;
@@ -183,27 +194,27 @@ TEST_CASE("rcFilterLowHangingWalkableObstacles", "[recast, filtering]")
 
 TEST_CASE("rcFilterLedgeSpans", "[recast, filtering]")
 {
-	rcContext context;
-	int walkableClimb = 5;
-	int walkableHeight = 10;
-
-	rcHeightfield heightfield;
-	heightfield.width = 10;
-	heightfield.height = 10;
-	heightfield.bmin[0] = 0;
-	heightfield.bmin[1] = 0;
-	heightfield.bmin[2] = 0;
-	heightfield.bmax[0] = 10;
-	heightfield.bmax[1] = 1;
-	heightfield.bmax[2] = 10;
-	heightfield.cs = 1;
-	heightfield.ch = 1;
-	heightfield.spans = (rcSpan**)rcAlloc(heightfield.width * heightfield.height * sizeof(rcSpan*), RC_ALLOC_PERM);
-	heightfield.pools = NULL;
-	heightfield.freelist = NULL;
-
 	SECTION("Edge spans are marked unwalkable")
 	{
+		rcContext context;
+		int walkableClimb = 5;
+		int walkableHeight = 10;
+
+		rcHeightfield heightfield;
+		heightfield.width = 10;
+		heightfield.height = 10;
+		heightfield.bmin[0] = 0;
+		heightfield.bmin[1] = 0;
+		heightfield.bmin[2] = 0;
+		heightfield.bmax[0] = 10;
+		heightfield.bmax[1] = 1;
+		heightfield.bmax[2] = 10;
+		heightfield.cs = 1;
+		heightfield.ch = 1;
+		heightfield.spans = (rcSpan**)rcAlloc(heightfield.width * heightfield.height * sizeof(rcSpan*), RC_ALLOC_PERM);
+		heightfield.pools = NULL;
+		heightfield.freelist = NULL;
+
 		// Create a flat plane.
 		for (int x = 0; x < heightfield.width; ++x)
 		{
@@ -248,6 +259,368 @@ TEST_CASE("rcFilterLedgeSpans", "[recast, filtering]")
 			for (int z = 0; z < heightfield.height; ++z)
 			{
 				rcFree(heightfield.spans[x + z * heightfield.width]);
+			}
+		}
+	}
+
+	SECTION("https://github.com/recastnavigation/recastnavigation/pull/672")
+	{
+		rcContext context;
+		int walkableClimb = 5;
+		int walkableHeight = 20;
+
+		rcHeightfield heightfield;
+		heightfield.width = 5;
+		heightfield.height = 5;
+		heightfield.bmin[0] = -28.799999237060546875f;
+		heightfield.bmin[1] = -4.097219944000244140625f;
+		heightfield.bmin[2] = 22.3999996185302734375f;
+		heightfield.bmax[0] = 3.2000000476837158203125f;
+		heightfield.bmax[1] = 11.045299530029296875f;
+		heightfield.bmax[2] = 54.40000152587890625f;
+		heightfield.cs = 0.20000000298023223876953125f;
+		heightfield.ch = 0.20000000298023223876953125f;
+		heightfield.spans = (rcSpan**)rcAlloc(heightfield.width * heightfield.height * sizeof(rcSpan*), RC_ALLOC_PERM);
+		heightfield.pools = NULL;
+		heightfield.freelist = NULL;
+
+		for (int z = 0; z < heightfield.height; ++z)
+			for (int x = 0; x < heightfield.height; ++x)
+				heightfield.spans[x + z * heightfield.width] = NULL;
+
+		{
+			rcTempVector<rcSpan*> spans;
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 4;
+				span->smax = 6;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 20;
+				span->smax = 22;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 24;
+				span->smax = 34;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 36;
+				span->smax = 40;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 41;
+				span->smax = 42;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 64;
+				span->smax = 71;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			for (rcSizeType i = 1; i < spans.size(); ++i)
+				spans[i - 1]->next = spans[i];
+			heightfield.spans[1 + 2 * 5] = spans[0];
+		}
+		{
+			rcTempVector<rcSpan*> spans;
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 4;
+				span->smax = 6;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 18;
+				span->smax = 23;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 24;
+				span->smax = 26;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 32;
+				span->smax = 34;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 36;
+				span->smax = 37;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 39;
+				span->smax = 42;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 69;
+				span->smax = 70;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			for (rcSizeType i = 1; i < spans.size(); ++i)
+				spans[i - 1]->next = spans[i];
+			heightfield.spans[2 + 1 * 5] = spans[0];
+		}
+		{
+			rcTempVector<rcSpan*> spans;
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 4;
+				span->smax = 6;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 18;
+				span->smax = 21;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 24;
+				span->smax = 34;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 36;
+				span->smax = 37;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 38;
+				span->smax = 42;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 65;
+				span->smax = 72;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			for (rcSizeType i = 1; i < spans.size(); ++i)
+				spans[i - 1]->next = spans[i];
+			heightfield.spans[2 + 2 * 5] = spans[0];
+		}
+		{
+			rcTempVector<rcSpan*> spans;
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 4;
+				span->smax = 6;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 17;
+				span->smax = 19;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 20;
+				span->smax = 24;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 25;
+				span->smax = 34;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 36;
+				span->smax = 38;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 66;
+				span->smax = 73;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			for (rcSizeType i = 1; i < spans.size(); ++i)
+				spans[i - 1]->next = spans[i];
+			heightfield.spans[3 + 2 * 5] = spans[0];
+		}
+		{
+			rcTempVector<rcSpan*> spans;
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 4;
+				span->smax = 6;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 18;
+				span->smax = 21;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 33;
+				span->smax = 34;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 36;
+				span->smax = 37;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 38;
+				span->smax = 42;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 43;
+				span->smax = 45;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 65;
+				span->smax = 67;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			{
+				rcSpan* span = (rcSpan*)rcAlloc(sizeof(rcSpan), RC_ALLOC_PERM);
+				span->area = 1;
+				span->smin = 69;
+				span->smax = 72;
+				span->next = NULL;
+				spans.push_back(span);
+			}
+			for (rcSizeType i = 1; i < spans.size(); ++i)
+				spans[i - 1]->next = spans[i];
+			heightfield.spans[2 + 3 * 5] = spans[0];
+		}
+
+		rcFilterLedgeSpans(&context, walkableHeight, walkableClimb, heightfield);
+
+		{
+			const std::vector<unsigned> areas = getAreas(heightfield.spans[2 + 1 * 5]);
+			REQUIRE_THAT(areas, Catch::Matchers::Equals(std::vector<unsigned>({1, 1, 1, 1, 1, 0, 0})));
+		}
+
+		{
+			const std::vector<unsigned> areas = getAreas(heightfield.spans[1 + 2 * 5]);
+			REQUIRE_THAT(areas, Catch::Matchers::Equals(std::vector<unsigned>({1, 1, 1, 1, 0, 0})));
+		}
+
+		{
+			const std::vector<unsigned> areas = getAreas(heightfield.spans[2 + 2 * 5]);
+			REQUIRE_THAT(areas, Catch::Matchers::Equals(std::vector<unsigned>({1, 1, 1, 1, 0, 1})));
+		}
+
+		{
+			const std::vector<unsigned> areas = getAreas(heightfield.spans[3 + 2 * 5]);
+			REQUIRE_THAT(areas, Catch::Matchers::Equals(std::vector<unsigned>({1, 1, 1, 1, 0, 0})));
+		}
+
+		{
+			const std::vector<unsigned> areas = getAreas(heightfield.spans[2 + 3 * 5]);
+			REQUIRE_THAT(areas, Catch::Matchers::Equals(std::vector<unsigned>({1, 1, 1, 1, 1, 0, 1, 0})));
+		}
+
+		for (int z = 0; z < heightfield.height; ++z)
+		{
+			for (int x = 0; x < heightfield.height; ++x)
+			{
+				rcSpan* span = heightfield.spans[x + z * heightfield.width];
+				while (span)
+				{
+					rcSpan* const prev = span;
+					span = span->next;
+					rcFree(prev);
+				}
 			}
 		}
 	}
